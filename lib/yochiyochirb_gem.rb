b604fea3f0c992189f855cd5d5eed4a590f1b92f
leemyeongho <l.m.myeongho.lee@gmail.com>
2015-08-24 14:33:05 +0200

    first commit

---
+++ lib/yochiyochirb_gem.rb
@@ -0,0 +1,8 @@
+require "yochiyochirb_gem/version"
+
+module YochiyochirbGem
+  def hello
+  	'hello'
+  end
+  module_function :hello
+end
---
+++ spec/yochiyochirb_gem_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe YochiyochirbGem do
+  it 'has a version number' do
+    expect(YochiyochirbGem::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
